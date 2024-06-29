39fad556540403e056f601e944e86c62d9bea036
JRichm <jblackenmusic@gmail.com>
2024-06-29 15:25:21 -0600

    cubes render to screen and add raycast functions

--- solution/voxelGE/main.cpp
+++ solution/voxelGE/main.cpp
@@ -3,7 +3,9 @@
 #include <glad/glad.h>
 #include <glfw3.h>
 
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
+#include <glm/gtx/normalize_dot.hpp>
 
 #include "shaders/shader.h"
 
@@ -32,6 +34,13 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
+
+
+
+glm::vec3 screenToWorldRay(float mouseX, float mouseY, const glm::mat4& view, const glm::mat4& projection, int screenWidth, int screenHeight);
+bool intersectRayWithCubeFace(const glm::vec3& rayOrigin, const glm::vec3& rayDir, const glm::vec3& cubePos, const glm::vec3& faceNormal, float& t);
+void highlightIntersectedFace(const glm::vec3& rayOrigin, const glm::vec3& rayDir, const glm::vec3& cubePos);
+void drawHighlightedFace(const glm::vec3& cubePos, const glm::vec3& faceNormal);
 
 
 int main() {
@@ -73,9 +82,8 @@ int main() {
 
 	// Define vertices and VAO
 	std::vector<Voxel> voxels = {
-		Voxel(glm::ivec3(10, 10, 10), glm::vec3(1.0f, 0.0f, 0.0f)),
-		Voxel(glm::ivec3(20, 20, 20), glm::vec3(0.0f, 1.0f, 0.0f)),
-		Voxel(glm::ivec3(30, 30, 30), glm::vec3(0.0f, 0.0f, 1.0f)),
+		Voxel(glm::ivec3(0, 0, 0), glm::vec3(0.25f, 0.57f, 0.98f)),
+		Voxel(glm::ivec3(1, 1, 1), glm::vec3(0.25f, 0.30f, 0.98f)),
 		// Add more vertices as needed
 	};
 
@@ -87,19 +95,18 @@ int main() {
 	// 	  meshes.push_back(mesh);
 	// }
 
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO;  //, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	//glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Voxel) * voxels.size(), voxels.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Voxel::vertices), Voxel::vertices, GL_STATIC_DRAW);
 
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, 0, nullptr, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Voxel), (void*)offsetof(Voxel, position));
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -108,16 +115,15 @@ int main() {
 
 	Box boundary(Point3D(0, 0, 0), Point3D(128, 128, 128));
 	Octree octree(boundary, 4);
-
-	// Voxel voxel1(glm::ivec3(10, 10, 10), glm::vec3(1.0f, 0.0f, 0.0f));
-	// Voxel voxel2(glm::ivec3(20, 20, 20), glm::vec3(0.0f, 1.0f, 0.0f));
-	// Voxel voxel3(glm::ivec3(30, 30, 30), glm::vec3(0.0f, 0.0f, 1.0f));
-
-	for (const auto & voxel : voxels) {
+	for (const auto& voxel : voxels) {
 		octree.insert(voxel);
 	}
 
 	std::cout << "Voxels inserted successfully." << std::endl;
+
+	// Voxel voxel1(glm::ivec3(10, 10, 10), glm::vec3(1.0f, 0.0f, 0.0f));
+	// Voxel voxel2(glm::ivec3(20, 20, 20), glm::vec3(0.0f, 1.0f, 0.0f));
+	// Voxel voxel3(glm::ivec3(30, 30, 30), glm::vec3(0.0f, 0.0f, 1.0f));
 
 
 	// main loop
@@ -132,12 +138,15 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		shader.use();
-		shader.setMat4("view", camera.GetViewMatrix());
-		shader.setMat4("projection", glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f));
+		shader.setMat4("view", camera.getViewMatrix());
+		shader.setMat4("projection", glm::perspective(glm::radians(camera.zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f));
 
-
-		// Draw voxels from octree
 		glBindVertexArray(VAO);
+
+		double mouseX, mouseY;
+		glfwGetCursorPos(window, &mouseX, &mouseY);
+		glm::vec3 rayDir = screenToWorldRay(mouseX, mouseY, camera.getViewMatrix(), glm::perspective(glm::radians(camera.zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f), SCR_WIDTH, SCR_HEIGHT);
+
 		for (const auto& voxel : octree.getVoxelsForRendering()) {
 			glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(voxel.position));
 			shader.setMat4("model", model);
@@ -145,6 +154,8 @@ int main() {
 
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
+
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -175,25 +186,22 @@ void processInput(GLFWwindow* window) {
 		glfwSetWindowShouldClose(window, true);
 
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.ProcessKeyboard(FORWARD, shifting, deltaTime);
+		camera.processKeyboard(FORWARD, shifting, deltaTime);
 
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.ProcessKeyboard(BACKWARD, shifting, deltaTime);
+		camera.processKeyboard(BACKWARD, shifting, deltaTime);
 
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.ProcessKeyboard(LEFT, shifting, deltaTime);
+		camera.processKeyboard(LEFT, shifting, deltaTime);
 
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.ProcessKeyboard(RIGHT, shifting, deltaTime);
+		camera.processKeyboard(RIGHT, shifting, deltaTime);
 
 	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
-		camera.ProcessKeyboard(UP, shifting, deltaTime);
+		camera.processKeyboard(UP, shifting, deltaTime);
 
 	if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
-		camera.ProcessKeyboard(DOWN, shifting, deltaTime);
-
-	if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
-		camera.ProcessKeyboard(RUN, shifting, deltaTime);
+		camera.processKeyboard(DOWN, shifting, deltaTime);
 
 	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
 		std::cout << "Mouse Click" << std::endl;
@@ -214,10 +222,69 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
 	lastX = xpos;
 	lastY = ypos;
 
-	camera.ProcessMouseMovement(xoffset, yoffset);
+	camera.processMouseMovement(xoffset, yoffset);
 }
 
 // whenever the mouse scroll wheel scrolls
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
-	camera.ProcessMouseScroll(yoffset);
+	camera.processMouseScroll(yoffset);
+}
+
+glm::vec3 screenToWorldRay(float mouseX, float mouseY, const glm::mat4& view, const glm::mat4& projection, int screenWidth, int screenHeight) {
+	float x = (2.0f * mouseX) / screenWidth - 1.0f;
+	float y = 1.0f - (2.0f * mouseY) / screenHeight;
+	float z = 1.0f;
+
+	glm::vec3 ray_nds = glm::vec3(x, y, z);
+	glm::vec4 ray_clip = glm::vec4(ray_nds, 1.0);
+	glm::vec4 ray_eye = glm::inverse(projection) * ray_clip;
+	ray_eye = glm::vec4(ray_eye.x, ray_eye.y, -1.0, 0.0);
+
+	glm::vec3 ray_wor = glm::vec3(glm::inverse(view) * ray_eye);
+	ray_wor = glm::normalize(ray_wor);
+
+	return ray_wor;
+}
+
+bool intersectRayWithCubeFace(const glm::vec3& rayOrigin, const glm::vec3& rayDir, const glm::vec3& cubePos, const glm::vec3& faceNormal, float& t) {
+	float denom = glm::dot(faceNormal, rayDir);
+	if (glm::abs(denom) > 1e-6) {
+		glm::vec3 p0l0 = cubePos - rayOrigin;
+		t = glm::dot(p0l0, faceNormal) / denom;
+		return (t >= 0);
+	}
+	return false;
+}
+
+void highlightIntersectedFace(const glm::vec3& rayOrigin, const glm::vec3& rayDir, const glm::vec3& cubePos) {
+	std::vector<glm::vec3> faceNormals = {
+		glm::vec3(0.0f, 0.0f, -1.0f),
+		glm::vec3(0.0f, 0.0f, 1.0f),
+		glm::vec3(-0.0f, -1.0f, 0.0f),
+		glm::vec3(0.0f, 1.0f, 0.0f),
+		glm::vec3(-1.0f, 0.0f, 0.0f),
+		glm::vec3(1.0f, 0.0f, 0.0f)
+	};
+
+	float t;
+	bool intersected = false;
+	glm::vec3 intersectedNormal;
+	for (const auto& normal : faceNormals) {
+		if (intersectRayWithCubeFace(rayOrigin, rayDir, cubePos + normal * 0.5f, normal, t)) {
+			intersected = true;
+			intersectedNormal = normal;
+			break;
+		}
+	}
+
+	if (intersected) {
+		// highlight the intersected face
+		// draw the face with a different color or outline it
+		drawHighlightedFace(cubePos, intersectedNormal);
+	}
+}
+
+void drawHighlightedFace(const glm::vec3& cubePos, const glm::vec3& faceNormal) {
+	// implement drawing code to highlight the face based on the normal direction
+	// this can involve setting a different color for the face vartices or drawing and outline
 }
