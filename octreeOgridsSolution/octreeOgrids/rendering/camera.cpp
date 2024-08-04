4fbc3a0c1abe3cdf93e8bc345ab7a72f1acd0034
JRichm <jblackenmusic@gmail.com>
2024-08-04 17:11:18 -0600

    add camera file

---
+++ octreeOgridsSolution/octreeOgrids/rendering/camera.cpp
@@ -0,0 +1,83 @@
+// #include "camera.h"
+// 
+#include <iostream>
+// #include <string>
+// 
+// #include <glad/glad.h>
+// #include <glfw3.h>
+// 
+// int Camera(GLFWwindow* window) {
+// 	
+// 
+// }
+
+
+// Camera.cpp
+#include "Camera.h"
+
+Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch)
+    : position(position), worldUp(up), yaw(yaw), pitch(pitch), front(glm::vec3(0.0f, 0.0f, -1.0f)),
+    movementSpeed(2.5f), mouseSensitivity(0.1f), zoom(45.0f) {
+    updateCameraVectors();
+}
+
+glm::mat4 Camera::getViewMatrix() {
+    return glm::lookAt(position, position + front, up);
+}
+
+void Camera::processKeyboard(Camera_Movement direction, bool shifting, float deltaTime) {
+    float velocity = movementSpeed * deltaTime;
+    if (shifting)
+        velocity *= 1.75f;
+
+    if (direction == FORWARD)
+        position += front * velocity;
+    if (direction == BACKWARD)
+        position -= front * velocity;
+    if (direction == LEFT)
+        position -= right * velocity;
+    if (direction == RIGHT)
+        position += right * velocity;
+    if (direction == UP)
+        position += up * velocity;
+    if (direction == DOWN)
+        position -= up * velocity;
+}
+
+void Camera::processMouseMovement(float xoffset, float yoffset, bool constrainPitch) {
+    xoffset *= mouseSensitivity;
+    yoffset *= mouseSensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (constrainPitch) {
+        if (pitch > 89.0f)
+            pitch = 89.0f;
+        if (pitch < -89.0f)
+            pitch = -89.0f;
+    }
+
+    updateCameraVectors();
+}
+
+void Camera::processMouseScroll(float yoffset) {
+    if (zoom >= 1.0f && zoom <= 45.0f)
+        zoom -= yoffset;
+    if (zoom <= 1.0f)
+        zoom = 1.0f;
+    if (zoom >= 45.0f)
+        zoom = 45.0f;
+}
+
+
+void Camera::updateCameraVectors() {
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    this->front = glm::normalize(front);
+
+    right = glm::normalize(glm::cross(this->front, worldUp));
+    up = glm::normalize(glm::cross(right, this->front));
+}
