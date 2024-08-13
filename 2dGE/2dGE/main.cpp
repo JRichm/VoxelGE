de5c7fd308ce119154a88b35a1b8028433e1c9d1
JRichm <jblackenmusic@gmail.com>
2024-08-12 23:22:07 -0600

    add main class to start opengl window

--- 2dGE/2dGE/main.cpp
+++ 2dGE/2dGE/main.cpp
@@ -1,154 +1,66 @@
-﻿#include <iostream>
-#include <string>
-#include <vector>
+#include <iostream>
 
-#include <windows.h>
+#include <glm/glm.hpp>
 
-#include <cstdlib>
-#include <conio.h>
+#include <glad/glad.h>
+#include <glfw3.h>
 
+#include "shaders/shader.h"
 
+GLFWwindow* window;
+bool running = false;
 
-// i need to output a matrix to the console
-// 1. first need to display just zeros, in a visual way
-// 2. need to figure out how to change specific values
-// 3. figure out how to change a specific value of the matrix
-// 4. set up different functions for each cell of the matrix to change others
-//		maybe set up a timer to actually see the changes
-// 5. set up the simulation
-
-
-// setting up the program to display the matrixes at first may cause some headache
-// when trying to display the matrix in open opengl
-// think about how we want to keep track of every cell, what other games do to handle
-// the large amount of pixels to keep track of
-// thinking of this, we should have a way of recording all of the cells of the matrix
-// and a separate part of the code that displays the cells.
-// this way we can work on each part of the engine and advance the techniques individually 
-
-
-// how can we display text to the console?
-// test and figure out how
-
-
-// CONWAYS GAME OF LIFE RULES
-
-	// 1) any live cell with < 2 live neighbors dies (underpopulation)
-	// 2) any live cell with two or three live neighbors lives
-	// 3) any live cell with more than three live neighbors dies (overpopulation)
-	// 4) any dead cell with exactly three live neighbors becomes a live cell (reproduction)
-
-
-
-class Grid {
-private:
-	std::vector<std::string> grid;
-	std::vector<std::string> bufferGrid;
-	int width, height;
-
-public:
-	Grid(int w, int h) : width(w), height(h) {
-		grid = clearedGrid(w, h);
-		bufferGrid = clearedGrid(w, h);
-
-		bufferGrid[25][24 * 2] = '*';
-		bufferGrid[25][24 * 2 + 1] = ' ';
-
-		bufferGrid[25][25 * 2] = '*';
-		bufferGrid[25][25 * 2 + 1] = ' ';
-
-		bufferGrid[25][26 * 2] = '*';
-		bufferGrid[25][26 * 2 + 1] = ' ';
-
-		bufferGrid[24][25 * 2] = '*';
-		bufferGrid[24][25 * 2 + 1] = ' ';
-
-		bufferGrid[26][26 * 2] = '*';
-		bufferGrid[26][26 * 2 + 1] = ' ';
-
-		grid = bufferGrid;
-	};
-
-	std::vector<std::string> clearedGrid(int w, int h) {
-		std::vector<std::string> clearedGrid = std::vector<std::string>(h, std::string(w * 2, '.'));
-		for (auto& row : clearedGrid) {
-			for (int i = 0; i < w; ++i) {
-				row[i * 2] = '.';
-				row[i * 2 + 1] = ' ';
-			}
-		};
-		return clearedGrid;
-	};
-
-	std::string getCell(int x, int y) const {
-		if (x >= 0 && x < height && y >= 0 && y < width) {
-			return grid[x].substr(y * 2, 2);
-		}
-		return "";
-	};
-
-	void setCell(int x, int y, char value) {
-		if (x >= 0 && x < height && y >= 0 && y < width) {
-			bufferGrid[x][y * 2] = value;
-			bufferGrid[x][y * 2 + 1] = ' ';
-		}
-	}
-
-	int countNeighbors(int x, int y) {
-		int count = 0;
-		for (int i = -1; i <= 1; ++i) {
-			for (int j = -1; j <= 1; ++j) {
-				if (i == 0 && j == 0) continue;
-				if (getCell(x + i, y + j) == "* ") {
-					count++;
-				}
-			}
-		}
-		return count;
-	}
-
-	void displayGrid() const {
-		system("cls");
-		for (const auto& row : grid) {
-			std::cout << row << '\n';
-		}
-	}
-
-	void updateGrid() {
-		bufferGrid = clearedGrid(height, width);
-
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				int count = countNeighbors(i, j);
-				std::string currentCell = getCell(i, j);
-
-				if (currentCell == "* ") {
-					if (count < 2 || count > 3) {
-						setCell(i, j, '.');
-					} else {
-						setCell(i, j, '*');
-					}
-				} else {
-					if (count == 3) {
-						setCell(i, j, '*');
-					}
-				}
-			}
-		}
-		grid = bufferGrid;
-	}
-};
+void processInput() {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		running = false;
+}
 
 int main() {
+	std::cout << "Starting 2dVoxGE..." << std::endl;
 
-	Grid grid(50, 50);
-
-	grid.displayGrid();
-
-	while (true) {
-		grid.updateGrid();
-		grid.displayGrid();
-		Sleep(1);
+	if (!glfwInit()) {
+		std::cout << "Failed to initialize GLFW" << std::endl;
+		return false;
 	}
-	return 1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(800, 800, "2dVoxGE", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return false;
+	}
+
+	glfwMakeContextCurrent(window);
+	//glfwSetWindowUserPointer(window, this); NOT SURE WHAT THIS DOES. THINK IT IS USED WHEN USING THE 'RENDERER' CLASS OR SIMILAR
+
+	//glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); CHANGE WINDOW SIZE
+	//glfwSetCursorPosCallback(window, mouse_callback);	MOUSE MOVEMENT
+	//glfwSetScrollCallback(window, scroll_callback); SCROLL
+
+	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); ENABLE MOUSE CURSUR INPUT
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return false;
+	}
+
+	//glEnable(GL_DEPTH_TEST);
+
+	Shader shader = Shader("shaders/vertex.glsl", "shaders/fragment.glsl");
+	shader.use();
+
+	running = true;
+
+	while (running) {
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		processInput();
+	}
 }
