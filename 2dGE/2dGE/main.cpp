4413dda13fd83c3221de98df88ecc2fe3c57a939
JRichm <jblackenmusic@gmail.com>
2024-08-14 23:29:59 -0600

    draw multiple squares random colors

--- 2dGE/2dGE/main.cpp
+++ 2dGE/2dGE/main.cpp
@@ -10,14 +10,14 @@
 GLFWwindow* window;
 bool running = false;
 
-
 float vertices[] = {
-	-0.3f, -0.3f, 0.0f,
-	0.3f, -0.3f, 0.0f,
-	0.3f, 0.3f, 0.0f,
-	-0.3f, 0.3f, 0.0f
+	0.0f, -0.1f, 0.0f,
+	0.1f, -0.1f, 0.0f,
+	0.1f, 0.0f, 0.0f,
+	0.0f, 0.0f, 0.0f
 };
 
+const int numSquares = 5;
 
 void processInput() {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -53,25 +53,68 @@ int main() {
 	Shader shader = Shader("shaders/vertex.glsl", "shaders/fragment.glsl");
 	shader.use();
 
-	GLuint VAO, VBO;
+	// random colors
+	glm::vec3 colors[numSquares];
+	for (unsigned int i = 0; i < numSquares; i++) {
+		colors[i] = glm::vec3(
+			static_cast <float> (rand()) / static_cast <float> (RAND_MAX),
+			static_cast <float> (rand()) / static_cast <float> (RAND_MAX),
+			static_cast <float> (rand()) / static_cast <float> (RAND_MAX)
+		);
+	}
 
+	// square positions
+	glm::mat4 models[numSquares];
+	for (unsigned int i = 0; i < numSquares; i++) {
+		float x = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 2.0f - 1.0f;
+		float y = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 2.0f - 1.0f;
+		models[i] = glm::translate(glm::mat4(1.0f), glm::vec3(x, y, 0.0f));
+	}
+
+	// generate buffers
+	GLuint VAO, VBO, colorVBO, modelVBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &colorVBO);
+	glGenBuffers(1, &modelVBO);
 
 	glBindVertexArray(VAO);
 
+	// vert buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // think this sets the vertex array offset, nothing until we get the array of vertices
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	// color buffer
+	glBindBuffer(GL_ARRAY_BUFFER, colorVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribDivisor(1, 1);
+
+	// model buffer
+	glBindBuffer(GL_ARRAY_BUFFER, modelVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(models), models, GL_STATIC_DRAW);
+
+	// add squares to shader
+	for (unsigned int i = 0; i < 4; i++) {
+		glVertexAttribPointer(2 + i, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(i * sizeof(glm::vec4)));
+		glEnableVertexAttribArray(2 + i);
+		glVertexAttribDivisor(2 + i, 1);
+	}
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
 	running = true;
 
+	// main loop
 	while (running) {
+		glBindVertexArray(VAO);
+		glDrawArraysInstanced(GL_TRIANGLE_FAN, 0, 4, numSquares);
+		glBindVertexArray(0);
+
 		if (glfwWindowShouldClose(window)) {
 			running = false;
 		}
@@ -81,7 +124,7 @@ int main() {
 
 		shader.use();
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+		glDrawArraysInstanced(GL_TRIANGLE_FAN, 0, 4, numSquares);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
@@ -91,6 +134,7 @@ int main() {
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &colorVBO);
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
