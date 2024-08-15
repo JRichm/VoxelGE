a7a2fd8c9551afe4acaf01287a9162eabe7c893d
JRichm <jblackenmusic@gmail.com>
2024-08-14 23:28:41 -0600

    update shaders to handle multiple squares and colors

--- 2dGE/2dGE/shaders/fragment.glsl
+++ 2dGE/2dGE/shaders/fragment.glsl
@@ -1,7 +1,8 @@
 #version 440 core
 out vec4 FragColor;
 
-void main()
-{
-    FragColor = vec4(1.0, 0.5, 0.2, 1.0); // Orange color
+in vec3 color;
+
+void main() {
+    FragColor = vec4(color, 1.0);
 }
--- 2dGE/2dGE/shaders/vertex.glsl
+++ 2dGE/2dGE/shaders/vertex.glsl
@@ -1,7 +1,11 @@
 #version 440 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 instanceColor;
+layout (location = 2) in mat4 model;
 
-void main()
-{
-    gl_Position = vec4(aPos, 1.0);
+out vec3 color;
+
+void main() {
+    gl_Position = model * vec4(aPos, 1.0);
+    color = instanceColor;
 }
